1828ceaa01dfe1f46a70a0d7e2a975449c31a58d
Charlie Powell <cdp1337@veraciousnetwork.com>
2023-05-13 21:08:08 -0400

    Many NPM changes and additions
    Add jsdoc for automatic documentation
    Add jest for test suites
    Add c8 for code coverage
    Upgrade rollup to newest version
    Add husky for pre-commit fun
    Add 'docs' task to generate documentation
    Add 'test' task to run test suites
    Add 'prepare' task to setup husky
    Add 'lint' task to lint source code

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,43 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import { eslint } from 'rollup-plugin-eslint';
-import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import eslint from '@rollup/plugin-eslint';
+import resolve from '@rollup/plugin-node-resolve';
+import { readFileSync } from 'fs';
 
-const { name, version, license, author, homepage } = require('./package.json');
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf8'));
 
-const banner = `/*! ${name} v${version} | ${license} (c) ${new Date().getFullYear()} ${author.name}, (c) 2021 Chris Diana | ${homepage} */`;
-
-const outputs = [];
-
-outputs.push({
-  file: 'dist/cms.js',
-  name: 'CMS',
-  format: 'iife',
-  banner: banner,
-});
-outputs.push({
-  file: 'dist/cms.es.js',
-  name: 'CMS',
-  format: 'es',
-  banner: banner,
-});
-
-export default {
+const banner = `/*! ${pkg.name} v${pkg.version} | ${pkg.license} (c) ${new Date().getFullYear()} ${pkg.author.name}, (c) 2021 Chris Diana | ${pkg.homepage} */`;
+export default [
+  {
     input: 'src/main.js',
-		external: ['CMS'],
-    output: outputs,
+    external: ['CMS'],
+    output: [
+      {
+        file: 'dist/cms.js',
+        name: 'CMS',
+        format: 'iife',
+        banner: banner,
+      },
+      {
+        file: 'examples/js/cms.js',
+        name: 'CMS',
+        format: 'iife',
+        banner: banner,
+      },
+      {
+        file: 'dist/cms.es.js',
+        name: 'CMS',
+        format: 'es',
+        banner: banner,
+      }
+    ],
     plugins: [
       eslint(),
       resolve(),
-			commonjs(),
-      babel({ exclude: 'node_modules/**' }),
+      //commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled'
+      }),
     ],
-};
+  }
+];
