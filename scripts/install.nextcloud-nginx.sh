72390338802d268847f584e044e30b193d71728b
Charlie Powell <cdp1337@veraciousnetwork.com>
2023-05-18 01:00:57 -0400

    Install script improvements

--- scripts/install.nextcloud-nginx.sh
+++ scripts/install.nextcloud-nginx.sh
@@ -2,6 +2,28 @@
 #
 # Installation script for NextCloud on an Debian-based server running nginx
 
+heading() {
+  echo ''
+  echo -e "\e[37m+=============================================================\e[0m"
+  echo -e "\e[37m| \e[35m${1}\e[0m"
+  echo -e "\e[37m+=============================================================\e[0m"
+}
+
+debug () {
+  echo -e "\e[37m ${1}\e[0m"
+}
+
+success () {
+  echo -e "\e[32m✓ ${1}\e[0m"
+}
+
+error () {
+  echo -e "\e[31m❌ ${1}\e[0m"
+}
+
+
+
+heading 'Configuration 1 of 3'
 # Prompt the user to enter their domain name
 DOMAIN=''
 while [ "$DOMAIN" == "" ]; do
@@ -21,6 +43,7 @@ else
 	SERVERLINE="${DOMAIN} www.${DOMAIN}"
 fi
 
+heading 'Configuration 2 of 3'
 # Ask of https should be enabled
 echo -n 'Enable HTTPS? (Y/n): '
 read HTTPS
@@ -33,7 +56,9 @@ case "$HTTPS" in
 		PROTO="https://";;
 esac
 
+heading 'Configuration 3 of 3'
 # Lookup if we can find the path automatically
+debug "Searching for ${DOMAIN}/nginx.conf..."
 MATCHES=$(find / -path "*/${DOMAIN}/nginx.conf" | wc -l)
 if [ "$MATCHES" -eq 1 ]; then
 	SITEPATH="$(find / -path "*/${DOMAIN}/nginx.conf" | xargs dirname)"
@@ -51,12 +76,13 @@ else
 		read SITEPATH
 
 		if [ ! -e "${SITEPATH}/nginx.conf" ]; then
-			echo "ERROR, ${SITEPATH}/nginx.conf not located"
+			error "${SITEPATH}/nginx.conf not located"
 			SITEPATH=""
 		fi
 	done
 fi
 
+heading 'Install Configuration 1 of 2'
 cat << EOT
 Edit nginx.conf and set the following lines at the start of the file
 (replacing the current values):
@@ -71,16 +97,17 @@ EOT
 
 READY=0
 while [ $READY -eq 0 ]; do
+	sleep 2
   if grep -q "set \$siteroot $SITEPATH;" "${SITEPATH}/nginx.conf"; then
 		if grep -q "set \$sitedomain $DOMAIN;" "${SITEPATH}/nginx.conf" ; then
 			READY=1
+			success 'nginx.conf configuration detected'
 		fi
 	fi
-
-	sleep 5
 done
 
 
+heading 'Install Configuration 2 of 2'
 cat << EOT
 Edit cgi-bin/config.ini and set the following line under "[site]"
 (replacing the current values):
@@ -94,21 +121,21 @@ EOT
 
 READY=0
 while [ $READY -eq 0 ]; do
+	sleep 2
 	if grep -q "host = ${PROTO}${DOMAIN}" "${SITEPATH}/cgi-bin/config.ini"; then
     READY=1
+    success 'config.ini configuration detected'
 	fi
-
-	sleep 5
 done
 
 
-echo "Setup ready!  Starting install"
+success "Setup ready!  Starting install"
 sleep 1
 
 # Configuration complete and ready to proceed!
 apt install fcgiwrap
 
-echo "Installing config to /etc/nginx/sites-enabled/${DOMAIN}.conf"
+debug "Installing config to /etc/nginx/sites-enabled/${DOMAIN}.conf"
 sleep 1
 # Setup nginx
 if [ $HTTPS -eq 0 ]; then
@@ -149,16 +176,19 @@ EOD
 fi
 
 
-echo "Testing config"
+heading 'Testing Config'
 sleep 1
 /sbin/nginx -t
 if [ $? -ne 0 ]; then
-	echo "ERROR, something's not right, automated script cannot continue."
+	error "something's not right, automated script cannot continue."
 	exit 1
 fi
+
+heading 'Restarting Services'
 systemctl restart nginx
 
 if [ $HTTPS -eq 1 ]; then
+  heading 'Installing Certbot/SSL'
 	if [ -z "$(which certbot)" ]; then
 		snap install --classic certbot
 		ln -s /snap/bin/certbot /usr/bin/certbot
@@ -168,4 +198,5 @@ if [ $HTTPS -eq 1 ]; then
 fi
 
 echo ""
-echo "Install script done!  If DNS is pointing here your site is accessible via ${PROTO}${DOMAIN}"
+success 'Install script complete'
+echo "If DNS is pointing here your site is accessible via ${PROTO}${DOMAIN}"
